dc8508ac1386a5d67992649efd92411232981b26
James Riley Wynne III <wynnejr@ornl.gov>
2014-09-12 12:59:08 -0400

    Rewrote to test transfer 3 times and to automate transfer of each size file. It seems to work

--- remoteTime.sh
+++ remoteTime.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 export TIMEFORMAT='%3R'
 TIMEFILE="time.log"
+SMALLFILE="smallFile.dat"
+MEDFILE="midFile.dat"
+BIGFILE="biggerFile.dat"
+AVGHSI=0
+AVGHTAR=0
+I=0
+declare -a files=($SMALLFILE $MEDFILE $BIGFILE)
+COUNTER=0
+INTCOUNTER=0
 cd $HOME/dataTiming
 export PATH=/sw/cave/tmux/1.7/centos5.8_gnu4.1.2/bin:/sw/cave/zsh/5.0.0/centos5.8_gnu4.1.2/bin:/usr/lib64/qt-3.3/bin:/usr/lib64/openmpi/bin:/sw/redhat6/lustredu/1.4/rhel6.5_gnu4.7.1/install/bin:/sw/home/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/public/bin
-pwd
 echo "Running small file tests on DTN"
-echo "HSI on DTN:"
-echo "HSI on DTN 1GB file: " >> $TIMEFILE
-{ time $(hsi put smallFile.dat 2> /dev/null) ; } >>  $TIMEFILE 2>&1
-echo "HTAR on DTN Node"
-echo "HTAR putting 1GB tarred file on HPSS from DTN: " >> $TIMEFILE
-{ time htar -cvf small.tar smallFile.dat >> /dev/null ; } >> $TIMEFILE 2>&1
+while [ $COUNTER -lt 3 ] ; do
+  while [ $INTCOUNTER -lt 3 ] ; do
+    echo "HSI ${files[$COUNTER]} on DTN"
+    I=$( { time $(hsi put ${files[$COUNTER]} 2> /dev/null); } 2>&1 )
+    AVGHSI=$(echo $AVGHSI + $I | bc)
+    echo "HTAR ${files[$COUNTER]} on DTN"
+    I=$( { time htar -cvf small.tar ${files[$COUNTER]} >> /dev/null; } 2>&1 )
+    AVGHTAR=$(echo $AVGHTAR + $I | bc)
+    #Average the time for this cycle
+    INTCOUNTER=$(echo $INTCOUNTER + 1 | bc)
+  done
 
-#Run Large file tests on DTN
-
+  AVGHSI=$(echo $AVGHSI / 3 | bc -l)
+  AVGHTAR=$(echo $AVGHTAR / 3 |bc -l)
+  echo "Average HSI time from DTN for ${files[$COUNTER]} after 3 iterations:" >> $TIMEFILE
+  echo $AVGHSI >> $TIMEFILE
+  echo "Average HSI for ${files[$COUNTER]} from DTN"
+  echo $AVGHSI
+  echo "Average HTAR time from DTN for ${files[$COUNTER]} after 3 iterations:" >> $TIMEFILE
+  echo $AVGHTAR >> $TIMEFILE
+  echo "Average HTAR for ${files[$COUNTER]} from DTN"
+  echo $AVGHTAR
+  AVGHSI=0
+  AVGHTAR=0
+  INTCOUNTER=0
+  I=0
+  COUNTER=$(echo $COUNTER + 1 | bc)
+done
--- transferWrapper.sh
+++ transferWrapper.sh
@@ -2,18 +2,48 @@
 
 export TIMEFORMAT='%3R'
 TIMEFILE="time.log"
-JOBNUM=""
+SMALLFILE="smallFile.dat"
+MEDFILE="midFile.dat"
+BIGFILE="biggerFile.dat"
+AVGHSI=0
+AVGHTAR=0
+I=0
+declare -a files=($SMALLFILE $MEDFILE $BIGFILE)
+COUNTER=0
+INTCOUNTER=0
 rm -f *.log
 touch time.log
-echo "Running small file tests:"
-echo "HSI on Titan Login Node"
-echo "hsi putting a 1GB file onto HPSS: " >> $TIMEFILE
-{ time $(hsi put smallFile.dat 2> /dev/null) ; } >> $TIMEFILE 2>&1
-echo "HTAR on Titan Login Node"
-echo "HTAR putting 1GB tarred file on HPSS from login: " >> $TIMEFILE
-{ time htar -cvf small.tar smallFile.dat >> /dev/null ; } >> $TIMEFILE 2>&1
 
-#Run Large file tests on Login
+echo "Running  tests:"
+while [ $COUNTER -lt 3 ] ; do
+  while [ $INTCOUNTER -lt 3 ] ; do
+    echo "HSI ${files[$COUNTER]} on Titan Login Node"
+    I=$( { time $(hsi put ${files[$COUNTER]} 2> /dev/null); } 2>&1 )
+    AVGHSI=$(echo $AVGHSI + $I | bc)
+    echo "HTAR ${files[$COUNTER]} on Titan Login Node"
+    I=$( { time htar -cvf small.tar ${files[$COUNTER]} >> /dev/null; } 2>&1 )
+    AVGHTAR=$(echo $AVGHTAR + $I | bc)
+    #Average the time for this cycle
+    INTCOUNTER=$(echo $INTCOUNTER + 1 | bc)
+  done
+  
+  AVGHSI=$(echo $AVGHSI / 3 | bc -l)
+  AVGHTAR=$(echo $AVGHTAR / 3 |bc -l)
+  echo "Average HSI time for ${files[$COUNTER]} after 3 iterations:" >> $TIMEFILE
+  echo $AVGHSI >> $TIMEFILE
+  echo "Average HSI for ${files[$COUNTER]}"
+  echo $AVGHSI
+  echo "Average HTAR time for ${files[$COUNTER]} after 3 iterations:" >> $TIMEFILE
+  echo $AVGHTAR >> $TIMEFILE
+  echo "Average HTAR for ${files[$COUNTER]}"
+  echo $AVGHTAR
+  AVGHSI=0
+  AVGHTAR=0
+  INTCOUNTER=0
+  I=0
+  COUNTER=$(echo $COUNTER + 1 | bc)
+done
+
 
 echo "Logging into DTN: "
 ssh dtn "bash -s" << 'ENDSSH'
