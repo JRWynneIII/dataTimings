f076e01de360ed1db824ebc77f98aa8956193c63
James Riley Wynne III <wynnejr@ornl.gov>
2014-10-29 11:54:46 -0400

    Adding ability to get htars true timing

--- remoteTime.sh
+++ remoteTime.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 export TIMEFORMAT='%3R'
-TIMEFILE="time.log"
-SMALLFILE="smallFile.dat"
-MEDFILE="midFile.dat"
-BIGFILE="biggerFile.dat"
+DATE=$(date +%s)
+SMALLFILE="/lustre/atlas/scratch/wyn/stf007/dataTiming/smallFile.dat"
+MEDFILE="/lustre/atlas/scratch/wyn/stf007/dataTiming/midFile.dat"
+BIGFILE="/lustre/atlas/scratch/wyn/stf007/dataTiming/biggerfile.dat"
+SUFFIX="dtn.log"
+TIMEFILE="/lustre/atlas/scratch/wyn/stf007/dataTiming/time/$DATE$SUFFIX"
+touch $TIMEFILE
 AVGHSI=0
 AVGHTAR=0
 I=0
@@ -12,14 +15,30 @@ COUNTER=0
 INTCOUNTER=0
 cd $HOME/dataTiming
 export PATH=/sw/cave/tmux/1.7/centos5.8_gnu4.1.2/bin:/sw/cave/zsh/5.0.0/centos5.8_gnu4.1.2/bin:/usr/lib64/qt-3.3/bin:/usr/lib64/openmpi/bin:/sw/redhat6/lustredu/1.4/rhel6.5_gnu4.7.1/install/bin:/sw/home/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/public/bin
+
+function clean {
+  hsi rm smallFile.dat 2> /dev/null
+  hsi rm midFile.dat 2> /dev/null
+  hsi rm biggerFile.dat 2> /dev/null
+  hsi rm file.tar 2> /dev/null
+  hsi rm 1TB.dat 2> /dev/null
+  hsi rm file.tar.idx 2> /dev/null
+}
+
 echo "Running file tests on DTN"
 while [ $COUNTER -lt 3 ] ; do
   while [ $INTCOUNTER -lt 3 ] ; do
+    clean
     echo "HSI ${files[$COUNTER]} on DTN"
     I=$( { time $(hsi put ${files[$COUNTER]} 2> /dev/null); } 2>&1 )
+    echo "Test $INTCOUNTER for ${files[$COUNTER]} With HSI on DTN" >> $TIMEFILE
+    echo $I >> $TIMEFILE
     AVGHSI=$(echo $AVGHSI + $I | bc)
+    clean
     echo "HTAR ${files[$COUNTER]} on DTN"
-    I=$( { time htar -cvf small.tar ${files[$COUNTER]} >> /dev/null; } 2>&1 )
+    I=$( { time htar -cvf file.tar ${files[$COUNTER]} >> /dev/null; } 2>&1 )
+    echo "Test $INTCOUNTER for ${files[$COUNTER]} With HTAR on DTN" >> $TIMEFILE
+    echo $I >> $TIMEFILE
     AVGHTAR=$(echo $AVGHTAR + $I | bc)
     #Average the time for this cycle
     INTCOUNTER=$(echo $INTCOUNTER + 1 | bc)
@@ -41,3 +60,13 @@ while [ $COUNTER -lt 3 ] ; do
   I=0
   COUNTER=$(echo $COUNTER + 1 | bc)
 done
+clean
+echo "HSI of 1TB.dat on Titan Login Node"
+I=$( { time $(hsi put /lustre/atlas/scratch/wyn/stf007/dataTiming/1TB.dat 2> /dev/null); } 2>&1 )
+echo "Test 1 for 1TB.dat with HSI" >> $TIMEFILE
+echo $I >> $TIMEFILE
+echo $I
+I=$( { time $(htar -cVf file.tar /lustre/atlas/scratch/wyn/stf007/dataTiming/1TB.dat 2> /dev/null); } 2>&1 )
+echo "Test 1 for 1TB.dat with HTAR" >> $TIMEFILE
+echo $I >> $TIMEFILE
+echo $I
--- transferWrapper.sh
+++ transferWrapper.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 export TIMEFORMAT='%3R'
-TIMEFILE="time.log"
+DATE=$(date +%s)
+SUFFIX="login.log"
+TIMEFILE="time/$DATE$SUFFIX"
+touch $TIMEFILE
 SMALLFILE="smallFile.dat"
 MEDFILE="midFile.dat"
-BIGFILE="biggerFile.dat"
+BIGFILE="biggerfile.dat"
 AVGHSI=0
 AVGHTAR=0
 I=0
@@ -13,22 +16,33 @@ COUNTER=0
 INTCOUNTER=0
 rm -f *.log
 touch time.log
+echo $TIMEFILE
 
-hsi rm smallFile.dat 2> /dev/null
-hsi rm midFile.dat 2> /dev/null
-hsi rm biggerFile.dat 2> /dev/null
-
-echo "Running  tests:"
-while [ $COUNTER -lt 3 ] ; do
+function clean {
   hsi rm smallFile.dat 2> /dev/null
   hsi rm midFile.dat 2> /dev/null
   hsi rm biggerFile.dat 2> /dev/null
+  hsi rm file.tar 2> /dev/null
+  hsi rm 1TB.dat 2> /dev/null
+  hsi rm file.tar.idx 2> /dev/null
+}
+
+
+echo "Running  tests:"
+while [ $COUNTER -lt 3 ] ; do
   while [ $INTCOUNTER -lt 3 ] ; do
+    clean
     echo "HSI ${files[$COUNTER]} on Titan Login Node"
-    I=$( { time $(hsi put ${files[$COUNTER]} 2> /dev/null); } 2>&1 )
+    I=$( { time $(hsi put ${files[$COUNTER]} 2> /dev/null); } 2>&1 ) 
+    echo "Test $INTCOUNTER for ${files[$COUNTER]} With HSI" >> $TIMEFILE
+    echo $I >> $TIMEFILE
+    hsi rm ${files[$COUNTER]} 2> /dev/null
     AVGHSI=$(echo $AVGHSI + $I | bc)
+    clean
     echo "HTAR ${files[$COUNTER]} on Titan Login Node"
-    I=$( { time htar -cvf small.tar ${files[$COUNTER]} >> /dev/null; } 2>&1 )
+    I=$( htar -cvf file.tar ${files[$COUNTER]} | grep -Po "(?<=time: ).*(?= seconds)" )
+    echo "Test $INTCOUNTER for ${files[$COUNTER]} With HTAR" >> $TIMEFILE
+    echo $I >> $TIMEFILE
     AVGHTAR=$(echo $AVGHTAR + $I | bc)
     #Average the time for this cycle
     INTCOUNTER=$(echo $INTCOUNTER + 1 | bc)
@@ -50,20 +64,27 @@ while [ $COUNTER -lt 3 ] ; do
   I=0
   COUNTER=$(echo $COUNTER + 1 | bc)
 done
+clean
+echo "HSI of 1TB.dat on Titan Login Node"
+I=$( { time $(hsi put 1TB.dat 2> /dev/null); } 2>&1 )
+echo "Test 1 for 1TB.dat with HSI" >> $TIMEFILE
+echo $I >> $TIMEFILE
+echo $I
+I=$( htar -cvf file.tar 1TB.dat | grep -Po "(?<=time: ).*(?= seconds)" )
+I=$( { time $(htar -cVf file.tar 1TB.dat 2> /dev/null); } 2>&1 )
+echo "Test 1 for 1TB.dat with HTAR" >> $TIMEFILE
+echo $I >> $TIMEFILE
+echo $I
 
-hsi rm smallFile.dat 2> /dev/null
-hsi rm midFile.dat 2> /dev/null
-hsi rm biggerFile.dat 2> /dev/null
+clean
 
 echo "Logging into DTN: "
 ssh dtn "bash -s" << 'ENDSSH'
-cd $HOME/dataTiming
+cd /lustre/atlas/scratch/wyn/stf007/dataTiming
 ./remoteTime.sh
 ENDSSH
 
-hsi rm smallFile.dat 2> /dev/null
-hsi rm midFile.dat 2> /dev/null
-hsi rm biggerFile.dat 2> /dev/null
+clean 
 
 echo "Running small file batch tests from titan login node"
 JOBNUM=$(qsub -q dtn hpss.pbs)
@@ -71,3 +92,5 @@ until [[ -f "dataTransferTimings.o$JOBNUM" ]] ;
   do
     sleep 2s
   done
+echo "BATCH DATA" >> $TIMEFILE
+cat dataTransferTimings.o$JOBNUM >> $TIMEFILE
