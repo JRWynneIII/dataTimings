6861bdbab0fdf1c4eb8724bb820ef5ae609ed771
James Riley Wynne III <wynnejr@ornl.gov>
2015-01-07 11:48:49 -0500

    Added file to test complex directory structures

--- complex.sh
+++ complex.sh
@@ -3,21 +3,19 @@
 export TIMEFORMAT='%10R'
 DATE=$(date +%s)
 SUFFIX="login.log"
-TIMEFILE="time/$DATE$SUFFIX"
+TIMEFILE="/lustre/atlas/scratch/wyn/stf007/dataTiming/time/$DATE$SUFFIX"
 touch $TIMEFILE
-SMALLFILE="smallFile.dat"
-MEDFILE="midFile.dat"
-BIGFILE="biggerfile.dat"
-HTARBIG="htardir"
-TBFILE="1TB.dat"
+SMALLFILE="1f"
+MEDFILE="2f"
+BIGFILE="3f"
 AVGHSI=0
 AVGHTAR=0
-declare -a files=($SMALLFILE $MEDFILE $BIGFILE $TBFILE)
-declare -a htarfiles=($SMALLFILE $MEDFILE $HTARBIG $TBFILE)
+declare -a files=($SMALLFILE $MEDFILE $BIGFILE)
 COUNTER=0
 INTCOUNTER=0
+SUBNUM=1
 rm -f *.log
-touch time.log
+touch /lustre/atlas/scratch/wyn/stf007/dataTimingi/time.log
 echo $TIMEFILE
 
 function clean {
@@ -31,34 +29,33 @@ function clean {
 
 
 function calcHSI {
-  I=$(hsi put ${files[$1]} 2>&1)
+  I=$(hsi put -R $1 2>&1)
   KBS=$(echo $I | grep -Po "(?<=bytes, ).*(?= KBS)")
-  BYTES=$(echo $I | grep -Po "(?<=/${files[$1]}' \( ).*(?= bytes)")
+  BYTES=$(echo $I | grep -Po "(?<=/$1' \( ).*(?= bytes)")
   KBYTES=$(echo $BYTES / 1000 | bc -l)
   HSITIME=$(echo $KBYTES / $(echo $KBS) | bc -l)
   echo $HSITIME
 }
 
+cd makedir
 echo "Running  tests:"
-while [ $COUNTER -lt 10 ] ; do
+while [ $COUNTER -lt 3 ] ; do
+  cd ${files[$COUNTER]}
   while [ $INTCOUNTER -lt 10 ] ; do
-    clean
-    echo "HSI ${files[$COUNTER]} on Titan Login Node"
-    I=$(calcHSI $COUNTER)
+    I=$(calcHSI "${files[$COUNTER]}$INTCOUNTER") 
     echo "Test $INTCOUNTER for ${files[$COUNTER]} With HSI" >> $TIMEFILE
     echo $I >> $TIMEFILE
-    hsi rm ${files[$COUNTER]} 2> /dev/null
+    echo $I
     AVGHSI=$(echo $AVGHSI + $I | bc)
-    clean
+    I=$( htar -cvf file.tar "${files[$COUNTER]}$INTCOUNTER" | grep -Po "(?<=time: ).*(?= seconds \()" )
     echo "HTAR ${htarfiles[$COUNTER]} on Titan Login Node"
-    I=$( htar -cvf file.tar ${htarfiles[$COUNTER]} | grep -Po "(?<=time: ).*(?= seconds \()" )
     echo "Test $INTCOUNTER for ${htarfiles[$COUNTER]} With HTAR" >> $TIMEFILE
     echo $I >> $TIMEFILE
     AVGHTAR=$(echo $AVGHTAR + $I | bc)
     #Average the time for this cycle
     INTCOUNTER=$(echo $INTCOUNTER + 1 | bc)
+    cd ..
   done
-  
   AVGHSI=$(echo $AVGHSI / 10 | bc -l)
   AVGHTAR=$(echo $AVGHTAR / 10 |bc -l)
   echo "Average HSI time for ${files[$COUNTER]} after 10 iterations:" >> $TIMEFILE
@@ -74,13 +71,4 @@ while [ $COUNTER -lt 10 ] ; do
   INTCOUNTER=0
   I=0
   COUNTER=$(echo $COUNTER + 1 | bc)
-done
-clean
-for COUNTER in {0..9}; do
-  echo "HSI test $COUNTER of 1TB.dat on Titan Login Node"
-  I=$(calcHSI 3)
-  echo "Test $COUNTER for 1TB.dat with HSI" >> $TIMEFILE
-  echo $I >> $TIMEFILE
-  echo $I
-  clean
 done
